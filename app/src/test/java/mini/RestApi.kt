69859e9469f593376268ee0db5491a11dc87a888
Tony Tang <tonytang@uber.com>
2024-04-11 12:28:52 -0700

    .execute().body()!!

--- app/src/test/java/mini/RestApi.kt
+++ app/src/test/java/mini/RestApi.kt
@@ -1,10 +1,14 @@
 package mini
 
+import io.reactivex.Single
 import retrofit2.http.GET
 import retrofit2.http.Header
 
 interface RestApi {
+    @GET("log_in")
+    fun login(@Header("x-bypass") flag: Int): Single<UserToken>
+
     @GET("auth")
-    fun login(@Header("x-bypass") flag: Int): retrofit2.Call<UserToken>
+    fun loginRetrofit2Call(@Header("x-bypass") flag: Int): retrofit2.Call<UserToken>
 
 }
