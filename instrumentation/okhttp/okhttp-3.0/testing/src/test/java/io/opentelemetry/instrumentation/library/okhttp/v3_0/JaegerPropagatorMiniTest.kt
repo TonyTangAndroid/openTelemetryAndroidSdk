40935271925a10b96a0bffae43f22bae34d945f7
Tony Tang <tonytang@uber.com>
2024-04-10 23:20:15 -0700

    inMemorySpanExporter.finishedSpanItems

--- instrumentation/okhttp/okhttp-3.0/testing/src/test/java/io/opentelemetry/instrumentation/library/okhttp/v3_0/JaegerPropagatorMiniTest.kt
+++ instrumentation/okhttp/okhttp-3.0/testing/src/test/java/io/opentelemetry/instrumentation/library/okhttp/v3_0/JaegerPropagatorMiniTest.kt
@@ -39,7 +39,7 @@ import java.io.IOException
 class JaegerPropagatorMiniTest {
     private val server = MockWebServer()
     private val restApi by lazy { RestApiUtil.restApi(server) }
-    private val inMemorySpanExporter: InMemorySpanExporter = InMemorySpanExporter.create()
+    private val spanExporter: InMemorySpanExporter = InMemorySpanExporter.create()
 
     @Before
     fun setup() {
@@ -60,7 +60,7 @@ class JaegerPropagatorMiniTest {
     fun teardown() {
         //clean up
         server.shutdown()
-        inMemorySpanExporter.reset()
+        spanExporter.reset()
         GlobalOpenTelemetry.resetForTest()
     }
 
@@ -73,7 +73,7 @@ class JaegerPropagatorMiniTest {
             assertRoot(rootSpan)
             Context.current().with(loggedInBaggage()).makeCurrent().use {
                 val loggedInSpan: Span = triggerLoggedInSpan(tracer, restApi)
-                assertLoggedIn(inMemorySpanExporter, server, loggedInSpan)
+                assertLoggedIn(spanExporter, server, loggedInSpan)
             }
         }
     }
@@ -82,7 +82,7 @@ class JaegerPropagatorMiniTest {
         //Make `uber-trace-id` attached.
         val jaegerPropagator: JaegerPropagator = JaegerPropagator.getInstance()
         val contextPropagators = ContextPropagators.create(jaegerPropagator)
-        val spanProcessor = SimpleSpanProcessor.create(inMemorySpanExporter)
+        val spanProcessor = SimpleSpanProcessor.create(spanExporter)
         val tracer = SdkTracerProvider.builder().addSpanProcessor(spanProcessor).build()
         val telemetrySdk = OpenTelemetrySdk.builder().setTracerProvider(tracer)
                 .setPropagators(contextPropagators)
@@ -134,22 +134,21 @@ class JaegerPropagatorMiniTest {
      * 4, Question so far: How could we associate the baggage with the tracing? By explicitly call ` Context.current()`?
      */
     private fun assertRoot(rootSpan: Span) {
-        val recordedRequest = server.takeRequest()
+        val request = server.takeRequest()
         //affirm
-        assertThat(recordedRequest.headers).hasSize(8)
-        val list: List<Pair<String, String>> = recordedRequest.headers.filter { it.first.startsWith("uberctx") }
+        assertThat(request.headers).hasSize(8)
+        val list: List<Pair<String, String>> = request.headers.filter { it.first.startsWith("uberctx") }
         assertThat(list).containsExactlyElementsIn(
                 listOf(Pair("uberctx-user.id", "321"), Pair("uberctx-user.name", "jack"))
         )
-        val uberTraceId = recordedRequest.headers["uber-trace-id"]
         val spanTraceId = rootSpan.spanContext.traceId
         //example value 8d828d3c7c8663418b067492675bef12
         assertThat(spanTraceId).isNotEmpty()
         //example value  8d828d3c7c8663418b067492675bef12:dae708107c50eb0f:0:1
-        assertThat(uberTraceId).isNotEmpty()
-        assertThat(uberTraceId).startsWith(spanTraceId)
-        assertThat(uberTraceId).isNotEqualTo("8d828d3c7c8663418b067492675bef12")
-        assertThat(uberTraceId).isEqualTo(assembleRawTraceId(inMemorySpanExporter.finishedSpanItems[0]))
+        assertThat(request.headers["uber-trace-id"]).isNotEmpty()
+        assertThat(request.headers["uber-trace-id"]).startsWith(spanTraceId)
+        assertThat(request.headers["uber-trace-id"]).isNotEqualTo("8d828d3c7c8663418b067492675bef12")
+        assertThat(request.headers["uber-trace-id"]).isEqualTo(assembleRawTraceId(spanExporter.finishedSpanItems[0]))
 
     }
 
