5c89f13028ab83dab18f623b3e0bd564ca0308f6
Tony Tang <tonytang@uber.com>
2024-04-11 12:35:16 -0700

    flag

--- app/src/main/java/ui/CheckInTracer.kt
+++ app/src/main/java/ui/CheckInTracer.kt
@@ -1,49 +1,21 @@
 package ui
 
 import app.DemoApp
-import io.opentelemetry.api.GlobalOpenTelemetry
 import io.opentelemetry.api.baggage.Baggage
-import io.opentelemetry.api.trace.Span
-import io.opentelemetry.api.trace.SpanBuilder
-import io.opentelemetry.api.trace.SpanKind
-import io.opentelemetry.api.trace.Tracer
 import io.opentelemetry.context.Context
-import io.reactivex.Single
 import network.UserStatus
-import repo.TokenRepo
 
 
 class CheckInTracer(private val context: android.content.Context) {
-    fun checkIn() {
-        val tracer: Tracer = GlobalOpenTelemetry.getTracer("CheckInTracer")
+    fun checkIn(): UserStatus? {
         Context.current().with(rootBaggage()).makeCurrent().use {
-            triggerRootSpan(tracer )
+            return checkInInternal()
         }
     }
 
-
-    private fun triggerRootSpan(tracer: Tracer ): Span {
-        val rootSpan: Span = rootSpan(tracer)
-        rootSpan.addEvent("start_logging_in")
-        //act
-        rootSpan.makeCurrent().use {
-            checkInInternal()
-        }
-        rootSpan.addEvent("finished_logging_in")
-        rootSpan.end()
-        return rootSpan
-    }
 
     private fun checkInInternal(): UserStatus? {
         return DemoApp.appScope(context).restApi().checkout().execute().body()
-    }
-
-
-    private fun rootSpan(tracer: Tracer): Span {
-        val spanBuilder: SpanBuilder = tracer.spanBuilder("A Test Span")
-        spanBuilder.setAttribute("root_key_1", "root_key_2")
-        spanBuilder.setSpanKind(SpanKind.CLIENT)
-        return spanBuilder.startSpan()
     }
 
 
