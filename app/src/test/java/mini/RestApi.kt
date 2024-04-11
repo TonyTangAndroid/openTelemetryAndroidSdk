f315044ebcac8648b4386c610b5581c7c253da3d
Tony Tang <tonytang@uber.com>
2024-04-11 12:20:14 -0700

    CheckInTracer

--- app/src/test/java/mini/RestApi.kt
+++ app/src/test/java/mini/RestApi.kt
@@ -7,6 +7,4 @@ interface RestApi {
     @GET("auth")
     fun login(@Header("x-bypass") flag: Int): retrofit2.Call<UserToken>
 
-    @GET("profile")
-    fun profile(@Header("token") flag: String): retrofit2.Call<UserStatus>
 }
