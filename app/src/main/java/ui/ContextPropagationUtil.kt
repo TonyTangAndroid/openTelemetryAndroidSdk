3714422439346cc18a642aafc3f7188afbae3b0a
Tony Tang <tonytang@uber.com>
2024-04-18 17:42:45 -0700

    `context`

--- app/src/main/java/ui/ContextPropagationUtil.kt
+++ app/src/main/java/ui/ContextPropagationUtil.kt
@@ -5,6 +5,14 @@ package ui
 import io.opentelemetry.api.baggage.Baggage
 import io.opentelemetry.context.Context
 
+/**
+ * 1, We provide the `context` source.
+ * 2, We provide `ContextPropagationUtil` to attach certain baggage such as uuid, cold launch id.
+ * 3, We revise the codegen to support receiving the  `context` as the parameter.
+ * 4, We are responsible to inject the Context into the network request as http header.
+ * 5, The main responsibility from feature developers
+ * is to pass along the Context across functions and features
+ */
 object ContextPropagationUtil {
 
     fun attachedCheckInStarted(): Baggage {
