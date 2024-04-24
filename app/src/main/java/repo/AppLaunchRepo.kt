9433917e61587d21cea6a801e266ab16ed48b86b
Tony Tang <tonytang@uber.com>
2024-04-24 14:40:39 -0700

    Completable

--- app/src/main/java/repo/AppLaunchRepo.kt
+++ app/src/main/java/repo/AppLaunchRepo.kt
@@ -5,13 +5,10 @@ import app.DemoApp
 import io.opentelemetry.api.baggage.Baggage
 import io.opentelemetry.context.Context
 import io.reactivex.Completable
-import io.reactivex.Single
-import network.CheckInResult
-import network.LocationModel
 
 class AppLaunchRepo(private val appContext: AppContext) {
 
-    fun checkingIn(locationModel: LocationModel, context: Context): Completable {
+    fun notifyAppLaunch(context: Context): Completable {
         val otelContext = context.with(attachedSendingNetwork(context))
         val token = TokenStore(appContext).token()
         return DemoApp.appScope(appContext).singleApi().appLaunch(otelContext,  token)
