e599eda0e1733c20eb450a738543a45c2bda8aab
Tony Tang <tonytang@uber.com>
2024-04-24 14:37:27 -0700

    appContext

---
+++ app/src/main/java/repo/CheckInRepo.kt
@@ -0,0 +1,38 @@
+@file:Suppress("OVERRIDE_DEPRECATION", "DEPRECATION")
+
+package repo
+
+import app.AppContext
+import app.DemoApp
+import io.opentelemetry.api.baggage.Baggage
+import io.opentelemetry.context.Context
+import io.reactivex.Single
+import network.CheckInResult
+import network.LocationModel
+import network.LogOutStatus
+
+class CheckInRepo(private val appContext: AppContext) {
+
+
+    fun checkingIn(locationModel: LocationModel, context: Context): Single<CheckInResult> {
+        val context1 = context.with(attachedSendingNetwork(context))
+        val token = TokenStore(appContext()).token()
+        return DemoApp.appScope(appContext()).singleApi().checkIn(context1, locationModel, token)
+    }
+
+
+    private fun loggingOut(): Single<LogOutStatus> {
+        return DemoApp.appScope(appContext()).singleApi().logOut()
+    }
+
+    private fun appContext() = appContext
+
+    private fun attachedSendingNetwork(context: Context): Baggage {
+        return Baggage.fromContext(context).toBuilder()
+                .put("sending_network", System.currentTimeMillis().toString())
+                .build()
+    }
+
+
+}
+
