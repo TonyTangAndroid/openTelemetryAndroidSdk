c7a7de777c185aaec34745eddd97204f0b4af6a5
Tony Tang <tonytang@uber.com>
2024-07-31 21:30:25 -0700

    TracingInterceptor.kt

--- app/src/main/java/okhttp_3_internal/TracingInterceptor.kt
+++ app/src/main/java/okhttp_3_internal/TracingInterceptor.kt
@@ -2,67 +2,59 @@
  * Copyright The OpenTelemetry Authors
  * SPDX-License-Identifier: Apache-2.0
  */
+package okhttp_3_internal
 
-package okhttp_3_internal;
-
-import java.io.IOException;
-
-import io.opentelemetry.context.Context;
-import io.opentelemetry.context.Scope;
-import io.opentelemetry.context.propagation.ContextPropagators;
-import io.opentelemetry.instrumentation.api.instrumenter.Instrumenter;
-import okhttp3.Interceptor;
-import okhttp3.Request;
-import okhttp3.Response;
+import io.opentelemetry.context.Context
+import io.opentelemetry.context.propagation.ContextPropagators
+import io.opentelemetry.instrumentation.api.instrumenter.Instrumenter
+import okhttp3.Interceptor
+import okhttp3.Request
+import okhttp3.Response
+import java.io.IOException
 
 /**
  * This class is internal and is hence not for public use. Its APIs are unstable and can change at
  * any time.
  */
-public final class TracingInterceptor implements Interceptor {
+class TracingInterceptor(
+    private val instrumenter: Instrumenter<Request, Response>,
+    private val propagators: ContextPropagators
+) : Interceptor {
+    @Throws(IOException::class)
+    override fun intercept(chain: Interceptor.Chain): Response {
+        var request: Request = chain.request()
+        val parentContext = Context.current()
 
-  private final Instrumenter<Request, Response> instrumenter;
-  private final ContextPropagators propagators;
+        if (!instrumenter.shouldStart(parentContext, request)) {
+            return chain.proceed(chain.request())
+        }
 
-  public TracingInterceptor(
-      Instrumenter<Request, Response> instrumenter, ContextPropagators propagators) {
-    this.instrumenter = instrumenter;
-    this.propagators = propagators;
-  }
+        val context = instrumenter.start(parentContext, request)
+        request = injectContextToRequest(request, context)
 
-  @Override
-  public Response intercept(Chain chain) throws IOException {
-    Request request = chain.request();
-    Context parentContext = Context.current();
-
-    if (!instrumenter.shouldStart(parentContext, request)) {
-      return chain.proceed(chain.request());
+        var response: Response? = null
+        var error: Throwable? = null
+        try {
+            context.makeCurrent().use {
+                response = chain.proceed(request)
+                return response!!
+            }
+        } catch (e: Exception) {
+            error = e
+            throw e
+        } finally {
+            instrumenter.end(context, request, response, error)
+        }
     }
 
-    Context context = instrumenter.start(parentContext, request);
-    request = injectContextToRequest(request, context);
-
-    Response response = null;
-    Throwable error = null;
-    try (Scope ignored = context.makeCurrent()) {
-      response = chain.proceed(request);
-      return response;
-    } catch (Exception e) {
-      error = e;
-      throw e;
-    } finally {
-      instrumenter.end(context, request, response, error);
+    // Context injection is being handled manually for a reason: we want to use the OkHttp Request
+    // type for additional AttributeExtractors provided by the user of this library
+    // thus we must use Instrumenter<Request, Response>, and Request is immutable
+    private fun injectContextToRequest(request: Request, context: Context): Request {
+        val requestBuilder: Request.Builder = request.newBuilder()
+        propagators
+            .textMapPropagator
+            .inject(context, requestBuilder, RequestHeaderSetter.INSTANCE)
+        return requestBuilder.build()
     }
-  }
-
-  // Context injection is being handled manually for a reason: we want to use the OkHttp Request
-  // type for additional AttributeExtractors provided by the user of this library
-  // thus we must use Instrumenter<Request, Response>, and Request is immutable
-  private Request injectContextToRequest(Request request, Context context) {
-    Request.Builder requestBuilder = request.newBuilder();
-    propagators
-        .getTextMapPropagator()
-        .inject(context, requestBuilder, RequestHeaderSetter.INSTANCE);
-    return requestBuilder.build();
-  }
 }
