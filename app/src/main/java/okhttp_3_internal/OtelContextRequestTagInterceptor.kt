a4f8b3f9925b1141b1b7496b9a052dbcab44a34c
Tony Tang <tonytang@uber.com>
2024-07-31 21:32:43 -0700

    val context = context(rawRequest)

--- app/src/main/java/okhttp_3_internal/OtelContextRequestTagInterceptor.kt
+++ app/src/main/java/okhttp_3_internal/OtelContextRequestTagInterceptor.kt
@@ -1,7 +1,5 @@
 package okhttp_3_internal
 
-import io.opentelemetry.api.GlobalOpenTelemetry
-import io.opentelemetry.context.Context
 import io.opentelemetry.context.propagation.TextMapSetter
 import okhttp3.Interceptor
 import okhttp3.Request
@@ -10,27 +8,10 @@ import java.io.IOException
 
 internal class OtelContextRequestTagInterceptor : Interceptor {
 
-    private val httpHeadersSetter = TextMapSetter<Request.Builder> { carrier, key, value -> carrier?.header(key, value) }
-
     @Throws(IOException::class)
     override fun intercept(chain: Interceptor.Chain): Response {
         val rawRequest = chain.request()
-        val context = context(rawRequest)
-        return if (context != null) {
-            chain.proceed(injectContext(context, rawRequest))
-        } else {
-            chain.proceed(rawRequest)
-        }
-    }
-
-    private fun context(rawRequest: Request): Context? {
-        return rawRequest.tag(Context::class.java)
-    }
-
-    private fun injectContext(context: Context, rawRequest: Request): Request {
-        val newBuilder: Request.Builder = rawRequest.newBuilder()
-        GlobalOpenTelemetry.get().propagators.textMapPropagator.inject(context, newBuilder, httpHeadersSetter)
-        return newBuilder.build()
+        return chain.proceed(rawRequest)
     }
 
 
