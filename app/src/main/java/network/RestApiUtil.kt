9fd6bec8b05ab74c336939676a524da3a191dc74
Tony Tang <tonytang@uber.com>
2024-07-31 21:20:58 -0700

    Add RestApiUtil.kt

--- app/src/main/java/network/RestApiUtil.kt
+++ app/src/main/java/network/RestApiUtil.kt
@@ -8,6 +8,7 @@ import com.google.gson.GsonBuilder
 import io.reactivex.schedulers.Schedulers
 import okhttp3.OkHttpClient
 import okhttp3.mockwebserver.MockWebServer
+import okhttp_3_android.OkHttp3Singletons
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
@@ -16,24 +17,27 @@ object RestApiUtil {
 
     fun retrofit(app: DemoApp, server: MockWebServer): Retrofit {
         val client: OkHttpClient = OkHttpClient.Builder()
-                .addInterceptor(FirstFixedInterceptor())
-                .addInterceptor(OtelContextRequestTagInterceptor())
-                .addInterceptor(ChuckerInterceptor.Builder(app).createShortcut(true).build())
-                .addInterceptor(SecondFixedInterceptor())
-                .build()
+            .addInterceptor(FirstFixedInterceptor())
+            .addInterceptor(OtelContextRequestTagInterceptor())
+            .addInterceptor(ChuckerInterceptor.Builder(app).createShortcut(true).build())
+            .addInterceptor(OkHttp3Singletons.TRACING_INTERCEPTOR)
+            .addInterceptor(SecondFixedInterceptor())
+            .build()
         return Retrofit.Builder()
-                .client(client)
-                .baseUrl(server.url("rt/v1/"))
-                .addConverterFactory(GsonConverterFactory.create(
-                        gson()
-                ))
-                .addCallAdapterFactory(RxJava2CallAdapterFactory.createWithScheduler(Schedulers.io()))
-                .build()
+            .client(client)
+            .baseUrl(server.url("rt/v1/"))
+            .addConverterFactory(
+                GsonConverterFactory.create(
+                    gson()
+                )
+            )
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.createWithScheduler(Schedulers.io()))
+            .build()
     }
 
     private fun gson(): Gson {
         return GsonBuilder()
-                .registerTypeAdapterFactory(BundleTypeAdapterFactory()).create()
+            .registerTypeAdapterFactory(BundleTypeAdapterFactory()).create()
     }
 
 }
