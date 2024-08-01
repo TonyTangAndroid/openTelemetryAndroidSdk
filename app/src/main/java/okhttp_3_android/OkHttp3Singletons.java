9d201306a68339161af2d07a9217a33bce4fc63a
Tony Tang <tonytang@uber.com>
2024-07-31 21:26:28 -0700

    Clean OkHttp3Singletons.java

--- app/src/main/java/okhttp_3_android/OkHttp3Singletons.java
+++ app/src/main/java/okhttp_3_android/OkHttp3Singletons.java
@@ -7,14 +7,8 @@ package okhttp_3_android;
 
 import static java.util.Collections.emptyList;
 
-import java.util.function.Supplier;
-
 import io.opentelemetry.api.GlobalOpenTelemetry;
-import io.opentelemetry.instrumentation.api.instrumenter.Instrumenter;
 import okhttp3.Interceptor;
-import okhttp3.Request;
-import okhttp3.Response;
-import okhttp_3_config.OkHttpInstrumentationConfig;
 import okhttp_3_internal.OkHttpInstrumenterFactory;
 import okhttp_3_internal.TracingInterceptor;
 
@@ -24,35 +18,21 @@ import okhttp_3_internal.TracingInterceptor;
  */
 public final class OkHttp3Singletons {
 
-    private static final Supplier<Instrumenter<Request, Response>> INSTRUMENTER =
-            CachedSupplier.create(
-                    () ->
-                            OkHttpInstrumenterFactory.create(
-                                    GlobalOpenTelemetry.get(),
-                                    builder ->
-                                            builder.setCapturedRequestHeaders(
-                                                            OkHttpInstrumentationConfig
-                                                                    .getCapturedRequestHeaders())
-                                                    .setCapturedResponseHeaders(
-                                                            OkHttpInstrumentationConfig
-                                                                    .getCapturedResponseHeaders())
-                                                    .setKnownMethods(
-                                                            OkHttpInstrumentationConfig
-                                                                    .getKnownMethods()),
-                                    spanNameExtractorConfigurer ->
-                                            spanNameExtractorConfigurer.setKnownMethods(
-                                                    OkHttpInstrumentationConfig.getKnownMethods()),
-                                    emptyList(),
-                                    OkHttpInstrumentationConfig
-                                            .emitExperimentalHttpClientMetrics()));
 
     public static final Interceptor TRACING_INTERCEPTOR =
-            new LazyInterceptor<>(
-                    CachedSupplier.create(
-                            () ->
-                                    new TracingInterceptor(
-                                            INSTRUMENTER.get(),
-                                            GlobalOpenTelemetry.getPropagators())));
+            new TracingInterceptor(
+                    OkHttpInstrumenterFactory.create(
+                            GlobalOpenTelemetry.get(),
+                            builder ->
+                            {
+                            },
+                            configure ->
+                            {
+                            },
+                            emptyList(),
+                            false),
+                    GlobalOpenTelemetry.getPropagators());
 
-    private OkHttp3Singletons() {}
+    private OkHttp3Singletons() {
+    }
 }
