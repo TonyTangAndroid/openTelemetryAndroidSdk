6da6b9fdfbf2e80b987fe778b348b0e4bdb4e9a2
Tony Tang <tonytang@uber.com>
2024-07-31 21:01:30 -0700

    Add OkHttpTelemetry.java

---
+++ app/src/test/java/okhttp_3_internal/RequestHeaderSetter.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package okhttp_3_internal;
+
+import javax.annotation.Nullable;
+
+import io.opentelemetry.context.propagation.TextMapSetter;
+import okhttp3.Request;
+
+/** Helper class to inject span context into request headers. */
+enum RequestHeaderSetter implements TextMapSetter<Request.Builder> {
+  INSTANCE;
+
+  @Override
+  public void set(@Nullable Request.Builder carrier, String key, String value) {
+    if (carrier == null) {
+      return;
+    }
+    carrier.header(key, value);
+  }
+}
