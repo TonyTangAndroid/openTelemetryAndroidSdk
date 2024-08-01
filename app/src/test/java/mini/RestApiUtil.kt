8b8a4f3ba5e05536cd11b3ae20332f74736ade64
Tony Tang <tonytang@uber.com>
2024-07-31 17:30:05 -0700

    Prepare to start

--- app/src/test/java/mini/RestApiUtil.kt
+++ app/src/test/java/mini/RestApiUtil.kt
@@ -1,6 +1,5 @@
 package mini
 
-import io.opentelemetry.instrumentation.library.okhttp.v3_0.internal.OkHttp3Singletons
 import io.reactivex.schedulers.Schedulers
 import okhttp3.OkHttpClient
 import okhttp3.mockwebserver.MockWebServer
