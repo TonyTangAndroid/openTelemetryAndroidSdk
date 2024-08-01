17d814ece0ac53a5ababcb3522e6f03699aee937
Tony Tang <tonytang@uber.com>
2024-07-31 21:25:08 -0700

    Clean OkHttp3Singletons.java

--- app/src/main/java/okhttp_3_android/OkHttp3Singletons.java
+++ app/src/main/java/okhttp_3_android/OkHttp3Singletons.java
@@ -10,15 +10,11 @@ import static java.util.Collections.emptyList;
 import java.util.function.Supplier;
 
 import io.opentelemetry.api.GlobalOpenTelemetry;
-import io.opentelemetry.context.Context;
-import io.opentelemetry.context.Scope;
 import io.opentelemetry.instrumentation.api.instrumenter.Instrumenter;
-import io.opentelemetry.instrumentation.api.semconv.http.HttpClientRequestResendCount;
 import okhttp3.Interceptor;
 import okhttp3.Request;
 import okhttp3.Response;
 import okhttp_3_config.OkHttpInstrumentationConfig;
-import okhttp_3_internal.ConnectionErrorSpanInterceptor;
 import okhttp_3_internal.OkHttpInstrumenterFactory;
 import okhttp_3_internal.TracingInterceptor;
 
@@ -49,33 +45,6 @@ public final class OkHttp3Singletons {
                                     emptyList(),
                                     OkHttpInstrumentationConfig
                                             .emitExperimentalHttpClientMetrics()));
-
-    public static final Interceptor CALLBACK_CONTEXT_INTERCEPTOR =
-            chain -> {
-                Request request = chain.request();
-                Context context =
-                        OkHttpCallbackAdviceHelper.tryRecoverPropagatedContextFromCallback(request);
-                if (context != null) {
-                    try (Scope ignored = context.makeCurrent()) {
-                        return chain.proceed(request);
-                    }
-                }
-
-                return chain.proceed(request);
-            };
-
-    public static final Interceptor RESEND_COUNT_CONTEXT_INTERCEPTOR =
-            chain -> {
-                try (Scope ignored =
-                        HttpClientRequestResendCount.initialize(Context.current()).makeCurrent()) {
-                    return chain.proceed(chain.request());
-                }
-            };
-
-    public static final Interceptor CONNECTION_ERROR_INTERCEPTOR =
-            new LazyInterceptor<>(
-                    CachedSupplier.create(
-                            () -> new ConnectionErrorSpanInterceptor(INSTRUMENTER.get())));
 
     public static final Interceptor TRACING_INTERCEPTOR =
             new LazyInterceptor<>(
